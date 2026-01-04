7f2a35d0bf4e9a1b7eab49fe455506243d156866
ConciergeServiceTurkiye <barbarosayhan@outlook.com>
2026-01-04 08:57:25 +0300

    Update restaurants.js

--- js/restaurants.js
+++ js/restaurants.js
@@ -197,7 +197,12 @@ if (childrenToggle && childrenToggle.checked) {
 
 
     const data = new FormData(form);
-    data.set("phone", iti.getNumber());
+
+data.set("phone", iti.getNumber());
+
+data.set("age_0_3", document.getElementById("age0_3")?.value || "");
+data.set("age_4_13", document.getElementById("age4_13")?.value || "");
+data.set("age_14_17", document.getElementById("age14_17")?.value || "");
     
     fetch("https://script.google.com/macros/s/AKfycbw9P03YjqbWBLy_YiGiJOUIL19uk89RmsSqWOt1CN3FV6WVqPg6IQFwjuj9RbBiYND7ZA/exec", {
   method: "POST",
@@ -237,6 +242,7 @@ itiObserver.observe(document.body, {
   childList: true,
   subtree: true
 });
+
 
 
 
